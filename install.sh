a253f96b2cebfdbf5351da509c133c69198afde2
R5whos <48851275+megalagger@users.noreply.github.com>
2020-04-16 10:41:33 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 clear
 echo "--------------------"
-echo "|      Choose      |"
+echo "|      Ты на?      |"
 echo "|------------------|"
 echo "| 1. Termux        |"
 echo "| 2. Unix          |"
 echo "|                  |"
-echo "| Choose 1 or 2:   |"
+echo "| choose 1 or 2:   |"
 echo "--------------------"
 read numb
 if [ $numb = "1" ]
@@ -17,21 +17,24 @@ then
 	pip3 install requests
 	pip3 install colorama
 	
-	spamer
 else
 	if [ $numb = "2" ]
 	then
 
 		if [ "$(whoami)" != 'root' ]; then
-			echo "Run install.sh whis root (sudo bash install.sh)"
+			echo "У вас нет прав. Запустите install.sh с root правами (sudo sh ~/spamer/install.sh)"
 			exit
 		else
 			apt install python3 python3-pip dos2unix
 			pip3 install requests
 			pip3 install colorama
+			cp ~/spamer/spammer.py /usr/bin/spamer
+			dos2unix /usr/bin/spamer
+			chmod 777 /usr/bin/spamer
 			spamer
 		fi
 	else
-		echo "Error!"
+		echo "Некорректный ввод"
 	fi
+fi
 fi
